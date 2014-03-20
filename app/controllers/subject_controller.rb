38fa5c95a5262a55546680e4fe47be814ff3c312
prabhakar <undurthi.prabhakar@gmail.com>
2014-03-20 13:46:48 +0530

    Implemented Date time in  Form helpers

--- app/controllers/subject_controller.rb
+++ app/controllers/subject_controller.rb
@@ -10,8 +10,9 @@ class SubjectController < ApplicationController
     @subject = Subject.find(params[:id])
   end
 
-  def news
+  def new
     @subject = Subject.new({:name => "Default"})
+    @subject_count = Subject.count + 1
   end
 
   def create
@@ -20,16 +21,18 @@ class SubjectController < ApplicationController
     # Save the object
     if @subject.save
       # If save succeeds, redirect to the index action
-      flash[:notice] = "Subject saved successfully."
+      flash[:notice] = "Subject created successfully."
       redirect_to(:action => 'index')
     else
       # If save fails, redisplay the form so user can fix problems
+      @subject_count = Subject.count + 1
       render('new')
     end
   end
 
   def edit
     @subject = Subject.find(params[:id])
+    @subject_count = Subject.count
   end
 
   def update
@@ -37,12 +40,12 @@ class SubjectController < ApplicationController
     @subject = Subject.find(params[:id])
     # Update the object
     if @subject.update_attributes(subject_params)
-
       # If update succeeds, redirect to the index action
       flash[:notice] = "Subject updated successfully."
       redirect_to(:action => 'show', :id => @subject.id)
     else
       # If update fails, redisplay the form so user can fix problems
+      @subject_count = Subject.count
       render('edit')
     end
   end
@@ -53,9 +56,8 @@ class SubjectController < ApplicationController
 
   def destroy
     subject = Subject.find(params[:id]).destroy
-    flash[:notice] = "Subject deleted successfully."
-
-   redirect_to(:action => 'index')
+    flash[:notice] = "Subject '#{subject.name}' destroyed successfully."
+    redirect_to(:action => 'index')
   end
 
 
@@ -65,7 +67,7 @@ class SubjectController < ApplicationController
       # same as using "params[:subject]", except that it:
       # - raises an error if :subject is not present
       # - allows listed attributes to be mass-assigned
-      params.require(:subject).permit(:name, :position, :visible)
+      params.require(:subject).permit(:name, :position, :visible, :created_at)
     end
 
 end
